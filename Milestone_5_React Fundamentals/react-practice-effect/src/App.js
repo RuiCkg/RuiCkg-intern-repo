d0b85513269c3c6c361d98eaf7996bd057fb3dfa
RuiCkg <liutiezuo@gmail.com>
2026-03-23 22:52:06 +1100

    useEffect

---
+++ Milestone_5_React Fundamentals/react-practice-effect/src/App.js
@@ -0,0 +1,12 @@
+import EffectExample from "./EffectExample";
+
+function App() {
+  return (
+    <div>
+      <h1>React Hooks Practice</h1>
+      <EffectExample />
+    </div>
+  );
+}
+
+export default App;
